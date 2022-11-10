a8fb16360eac83b66c9ac0ddbb9743e7577312b3
DennisHvA <dennis.de.graaf2@hva.nl>
2022-11-10 22:34:32 +0100

    api works with key in env

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ import '../styles/style.scss'
 
 // We can use node_modules directely in the browser!
 import * as d3 from 'd3';
+const KEY = import.meta.env.VITE_API_KEY
 
 console.log('main.js is linked');
 
@@ -19,3 +20,16 @@ fetch('./groups.json')
         console.log(newArray);
         
     });
+
+    // const options = {
+    //     method: 'GET',
+    //     headers: {
+    //         'X-RapidAPI-Key': KEY,
+    //         'X-RapidAPI-Host': 'football98.p.rapidapi.com'
+    //     }
+    // };
+    
+    // fetch('https://football98.p.rapidapi.com/fifaworldcup/table', options)
+    //     .then(response => response.json())
+    //     .then(response => console.log(response))
+    //     .catch(err => console.error(err));
